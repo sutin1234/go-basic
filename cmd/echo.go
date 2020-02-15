a12635c356c4dfa04fd657c073a9c0a1a01cd715
sutin1234 <tony.sutin1234@gmail.com>
2020-02-16 02:07:01 +0700

    add Echo GET Method With param

--- cmd/echo.go
+++ cmd/echo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"hello/oscar"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -17,7 +18,8 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Router
-	e.GET("oscarmale", OsCarMaleHandle)
+	e.GET("/oscarmale", OsCarMaleHandle)
+	e.GET("/fizzbuzz/:number", FizzBussHandle)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -26,4 +28,10 @@ func main() {
 func OsCarMaleHandle(c echo.Context) error {
 	result := oscar.ActorWhoGotMoreThanOne("../oscar/oscar_age_male.csv")
 	return c.JSON(http.StatusOK, result)
+}
+
+func FizzBussHandle(c echo.Context) error {
+	numberString := c.Param("number") // param value
+	n, _ := strconv.Atoi(numberString)
+	return c.JSON(http.StatusOK, n)
 }
